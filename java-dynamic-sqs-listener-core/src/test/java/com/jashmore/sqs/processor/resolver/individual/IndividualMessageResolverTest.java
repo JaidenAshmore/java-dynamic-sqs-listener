b844c08e5464d34afbade57e25c1c31787842148
Jaiden Ashmore <jaidenkyleashmore@gmail.com>
2019-02-05 11:42:28 -0600

    42: Change back to pitest because of testing performance (#43)
    * 42: Change back to pitest because of testing performance
    
    Fixed up IndividualMessageResolver mutation coverage
    Still need to fix log messages showing up in tests
    
    * refs #42: Actually run mutation testing

--- java-dynamic-sqs-listener-core/src/test/java/com/jashmore/sqs/processor/resolver/individual/IndividualMessageResolverTest.java
+++ java-dynamic-sqs-listener-core/src/test/java/com/jashmore/sqs/processor/resolver/individual/IndividualMessageResolverTest.java
@@ -1,8 +1,11 @@
 package com.jashmore.sqs.processor.resolver.individual;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static uk.org.lidalia.slf4jtest.LoggingEvent.debug;
+import static uk.org.lidalia.slf4jtest.LoggingEvent.error;
 
 import com.jashmore.sqs.QueueProperties;
 import org.junit.Before;
@@ -15,6 +18,8 @@ import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 import software.amazon.awssdk.services.sqs.model.DeleteMessageRequest;
 import software.amazon.awssdk.services.sqs.model.DeleteMessageResponse;
 import software.amazon.awssdk.services.sqs.model.Message;
+import uk.org.lidalia.slf4jtest.TestLogger;
+import uk.org.lidalia.slf4jtest.TestLoggerFactory;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -72,5 +77,38 @@ public class IndividualMessageResolverTest {
                 .queueUrl("queueUrl")
                 .receiptHandle("receipt2")
                 .build());
+    }
+
+    @Test
+    public void successfulResolvingOfMessageWillLogDebugMessage() {
+        // arrange
+        final TestLogger logger = TestLoggerFactory.getTestLogger(IndividualMessageResolver.class);
+        when(sqsAsyncClient.deleteMessage(any(DeleteMessageRequest.class)))
+                .thenReturn(CompletableFuture.completedFuture(DeleteMessageResponse.builder().build()));
+
+        // act
+        individualMessageResolver.resolveMessage(Message.builder().messageId("id").body("test").receiptHandle("receipt").build());
+
+        // assert
+        assertThat(logger.getAllLoggingEvents()).contains(debug("Message successfully deleted: {}", "id"));
+    }
+
+    @Test
+    public void failureToResolveMessagesResultsInErrorMessage() {
+        // arrange
+        final TestLogger logger = TestLoggerFactory.getTestLogger(IndividualMessageResolver.class);
+        final RuntimeException exception = new RuntimeException("exception");
+        when(sqsAsyncClient.deleteMessage(any(DeleteMessageRequest.class)))
+                .thenAnswer(invocation -> {
+                    final CompletableFuture<?> future = new CompletableFuture<>();
+                    future.completeExceptionally(exception);
+                    return future;
+                });
+
+        // act
+        individualMessageResolver.resolveMessage(Message.builder().messageId("id").body("test").receiptHandle("receipt").build());
+
+        // assert
+        assertThat(logger.getAllLoggingEvents()).contains(error(exception, "Error deleting message: id"));
     }
 }
