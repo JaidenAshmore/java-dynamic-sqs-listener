73b3124b939ccdf4a338caf54c440b00fcfc5488
Jaiden Ashmore <jashmore@atlassian.com>
2019-02-05 19:58:56 -0600

    refs #42: Increased mutation coverage to pass builds

--- java-dynamic-sqs-listener-core/src/test/java/com/jashmore/sqs/util/ResizableSemaphoreTest.java
+++ java-dynamic-sqs-listener-core/src/test/java/com/jashmore/sqs/util/ResizableSemaphoreTest.java
@@ -1,6 +1,7 @@
 package com.jashmore.sqs.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
@@ -45,7 +46,17 @@ public class ResizableSemaphoreTest {
     @Test
     public void changingPermitSizeToSameAmountWhilstSomeAcquiredDoesNothing() throws InterruptedException {
         // arrange
-        final ResizableSemaphore resizableSemaphore = new ResizableSemaphore(1);
+        final ResizableSemaphore resizableSemaphore = new ResizableSemaphore(1) {
+            @Override
+            public void release(final int permits) {
+                fail("Should not have released 0 permits");
+            }
+
+            @Override
+            protected void reducePermits(final int reduction) {
+                fail("Should not have reduced permits by zero");
+            }
+        };
         resizableSemaphore.acquire();
 
         // act
@@ -54,6 +65,52 @@ public class ResizableSemaphoreTest {
         // assert
         assertThat(resizableSemaphore.availablePermits()).isEqualTo(0);
         resizableSemaphore.release();
+        assertThat(resizableSemaphore.availablePermits()).isEqualTo(1);
+    }
+
+    @Test
+    public void increasingPermitSizeShouldReleaseTheNumberOfExtraPermits() throws InterruptedException {
+        // arrange
+        final ResizableSemaphore resizableSemaphore = new ResizableSemaphore(1) {
+            @Override
+            public void release(final int permits) {
+                assertThat(permits).isEqualTo(5);
+                super.release(permits);
+            }
+
+            @Override
+            protected void reducePermits(final int reduction) {
+                fail("Should not have reduced permits");
+            }
+        };
+
+        // act
+        resizableSemaphore.changePermitSize(6);
+
+        // assert
+        assertThat(resizableSemaphore.availablePermits()).isEqualTo(6);
+    }
+
+    @Test
+    public void decreasePermitSizeShouldReduceTheNumberOfExtraPermits() throws InterruptedException {
+        // arrange
+        final ResizableSemaphore resizableSemaphore = new ResizableSemaphore(6) {
+            @Override
+            public void release(final int permits) {
+                fail("Should not have released permits");
+            }
+
+            @Override
+            protected void reducePermits(final int reduction) {
+                assertThat(reduction).isEqualTo(5);
+                super.reducePermits(reduction);
+            }
+        };
+
+        // act
+        resizableSemaphore.changePermitSize(1);
+
+        // assert
         assertThat(resizableSemaphore.availablePermits()).isEqualTo(1);
     }
 }
