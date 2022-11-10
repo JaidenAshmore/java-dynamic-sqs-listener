6264149b53be41b7f8b989c30d06b1e0328b4ec5
Zane Williamson <zane.williamson@varomoney.com>
2022-11-10 10:44:15 -0800

    feat: import auth

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -5,5 +5,6 @@ val awsVersion: String by project
 dependencies {
     api(platform("software.amazon.awssdk:bom:${awsVersion}"))
     api("software.amazon.awssdk:sqs")
+    api("software.amazon.awssdk:auth")
     compileOnly(project(":documentation-annotations"))
 }
