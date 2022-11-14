493268812ffc2eefdac3c296eb40e28617da9f2a
Zane Williamson <zane.williamson@varomoney.com>
2022-11-14 13:40:00 -0800

    fix: include auth

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -4,7 +4,7 @@ val awsVersion: String by project
 
 dependencies {
     api(platform("software.amazon.awssdk:bom:${awsVersion}"))
+    api(platform("software.amazon.awssdk:auth:${awsVersion}"))
     api("software.amazon.awssdk:sqs")
     compileOnly(project(":documentation-annotations"))
-    api(implementation("com.amazonaws:aws-java-sdk-sts:${awsVersion}"))
 }
